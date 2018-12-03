bc723dd33186c930daefc0d1e2c0179047bf2e81
doums <dommerc.pierre@gmail.com>
2018-12-04 00:32:30 +0100

    wip

--- app/src/main/java/run/drop/app/LauncherActivity.kt
+++ app/src/main/java/run/drop/app/LauncherActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
